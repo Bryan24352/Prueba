4026089916339072d42d73b2c8f9003df820db17
Bryan24352 <bryan.2001322@gmail.com>
2022-10-28 19:20:32 -0600

    pido perdon por los anteriores commits  me cuesta un poco todavia

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 nombre: Bryan Alejandro Aguilar Calderon 
 numero de cuenta: 62021218
 prueba 1
+
+pido perdon de antemano ya que hice muchos commits 
+y por que subi erroneamente la carpeta node_modules
+mus disculpas ingeniero
--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
+const host = 'localhost';
+const port = 8000;
+/*onst express = require('express')
+const app = express()*/
 
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("Esto deberia estar funcionando");
+}
+/*
 app.listen(process.env.PORT,function(){
     console.log("El puerto esta corriendo el puerto TCP" + process.env.PORT)
-})
+})*/
 
 function multiplicar(j){
 
-	document.write("Tabla de multiplicar del "+ j +"");
+	console.log("Tabla de multiplicar del "+ j +"");
 
 	for(i = 1;i<=10;i++){
-		document.write(j + "x " + i + "= " + j * i);
+		console.log(j + "x " + i + "= " + j * i);
 
 	}
 
